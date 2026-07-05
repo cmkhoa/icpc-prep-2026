10f3766ddfd469bb145b4c5d627cfc036bf7fd4c
GodLYEI <thaituan3801@gmail.com>
2026-07-05 00:03:29 -0500

    solves distinct values queries

---
+++ 02-data-structures/Segment-Tree-Point-Update-Range-Query/Distinct-Values-Queries.cpp
@@ -0,0 +1,115 @@
+// https://cses.fi/problemset/task/1734
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 2e5 + 5;
+const int M = 4 * N; 
+
+int a[N];
+
+struct SegTree{
+    int size;
+    vector<int> nodes;
+
+    SegTree(int n){
+        size = 1;
+        while(size < n){
+            size *= 2;
+        }
+
+        nodes.assign(2 * size, 0);
+    }
+
+    // [l, r)
+    void set(int pos, int value, int id, int l, int r){
+        if (pos < l || pos >= r) return;
+        if (r - l == 1){
+            nodes[id] = value;
+            return;
+        }
+
+        int mid = (l + r) >> 1;
+
+        set(pos, value, 2 * id + 1, l, mid);
+        set(pos, value, 2 * id + 2, mid, r);
+
+        nodes[id] = nodes[2 * id + 1] + nodes[2 * id + 2]; // change this depending on problems
+    }
+
+    void set(int pos, int value){
+        set(pos, value, 0, 0, size);
+    }
+    
+    // (l, r)
+    int get(int u, int v, int id, int l, int r){
+        if (l >= v || r <= u){
+            return 0;
+        }
+        if (r <= v && l >= u){
+            return nodes[id];
+        }
+        
+        int mid = (l + r) >> 1;
+        
+        return get(u, v, 2 * id + 1, l, mid) + get(u, v, 2 * id + 2, mid, r);
+    }
+
+    int get(int u, int v){
+        return get(u, v, 0, 0, size);
+    }
+};  
+
+struct Query{
+    int l, r, i;
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, q;
+    cin >> n >> q;
+
+    for (int i = 0; i < n; i++){
+        cin>> a[i];
+    }
+
+    vector<Query> query;
+    for (int i = 0; i < q; i++){
+        int l, r;
+        cin >> l >> r;
+        l--;
+        
+        query.push_back({l, r, i});
+    }
+
+    sort(query.begin(), query.end(), [](const Query &a, const Query &b){
+        return a.r < b.r;
+    });
+    
+    SegTree st(n);
+    map<int, int> seen;
+
+    vector<int> res(q, 0);
+
+    int i = 0;
+    for (int j = 0; j < q; j++){
+        while(i < n && i < query[j].r){
+            if (seen.count(a[i])){
+                st.set(seen[a[i]], 0);
+            }
+            seen[a[i]] = i;
+            st.set(seen[a[i]], 1);
+
+            i++;
+        }
+
+        res[query[j].i] = st.get(query[j].l, query[j].r);
+    }
+
+    for (int i = 0; i < q; i++)
+        cout << res[i] << '\n';
+
+    return 0;
+}
