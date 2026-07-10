8299b6de7b23e19ea1dd5519cbcd84c400299497
Van Thai <126876930+tn-van-thai@users.noreply.github.com>
2026-07-10 00:55:20 -0500

    Update Interesting-Array problem link.cpp

--- 02-data-structures/Segment-Tree-Point-Update-Range-Query/Interesting-Array.cpp
+++ 02-data-structures/Segment-Tree-Point-Update-Range-Query/Interesting-Array.cpp
@@ -1,4 +1,4 @@
-// https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/A
+// https://codeforces.com/contest/482/problem/B
 
 #include <bits/stdc++.h>
 using namespace std;
